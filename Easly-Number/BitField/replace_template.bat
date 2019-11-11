e034e430c0e7b01ce94ee3c2ecce35bc615ebd16
dlebansais <dlebansais@gmail.com>
2019-11-11 11:59:57 +0100

    Fixed BitField template.

--- Easly-Number/BitField/replace_template.bat
+++ Easly-Number/BitField/replace_template.bat
@@ -1,3 +1,11 @@
 rem echo Replacing in folder '%~1'
-powershell -Command "(gc %~1/BitField_Template.cstext) -replace 'Template', 'uint' | Out-File -encoding UTF8 %~1/BitField_uint.cs"
-powershell -Command "(gc %~1/BitField_Template.cstext) -replace 'Template', 'byte' | Out-File -encoding UTF8 %~1/BitField_byte.cs"
+
+powershell -Command "(gc %~1\BitField_Template.cstext) -replace 'Template', 'uint' | Out-File -encoding UTF8 %~1\BitField_uint.cs.temp"
+fc /B "%~1\BitField_uint.cs.temp" "%~1\BitField_uint.cs" > nul
+if ERRORLEVEL 1 copy "%~1\BitField_uint.cs.temp" "%~1\BitField_uint.cs" > nul
+del "%~1\BitField_uint.cs.temp"
+
+powershell -Command "(gc %~1\BitField_Template.cstext) -replace 'Template', 'byte' | Out-File -encoding UTF8 %~1\BitField_byte.cs.temp"
+fc /B "%~1\BitField_byte.cs.temp" "%~1\BitField_byte.cs" > nul
+if ERRORLEVEL 1 copy "%~1\BitField_byte.cs.temp" "%~1\BitField_byte.cs" > nul
+del "%~1\BitField_byte.cs.temp"
